85c8ed097a50b079020fd1fb6a46689fe8941348
Kirua <yo44prg@icloud.com>
2022-10-12 10:53:17 +0200

    feat: expose number method

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const letter = require('./src/letter');
 const object = require('./src/object');
+const number = require('./src/number');
 
 module.exports = {
   letter,
-  object
+  object,
+  number
 }
